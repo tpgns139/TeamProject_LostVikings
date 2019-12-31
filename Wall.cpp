4dbeedd23a2c82267c7084e7b26f696822a50eac
tpgns139 <42164539+tpgns139@users.noreply.github.com>
2019-12-31 12:44:59 +0900

    Plus map Rect

--- Wall.cpp
+++ Wall.cpp
@@ -16,3 +16,15 @@ HRESULT Wall::init(mapInfo info)
 	MapObj::init(info);
 	return S_OK;
 }
+
+void Wall::update()
+{
+	_mapInfo._rc = RectMake(_mapInfo._x - CAMERA->getCameraXpos(),
+		_mapInfo._y - CAMERA->getCameraYpos(),
+		_mapInfo.width,
+		_mapInfo.height);
+}
+void Wall::render()
+{
+	Rectangle(_backBuffer->getMemDC(), _mapInfo._rc);
+}
